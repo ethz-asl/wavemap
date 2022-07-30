42d62c35a3c4ae2f83b8a307c8d0ffca4a6acc1b
Victor Reijgwart <victor_reijgwart@hotmail.com>
2022-07-30 19:50:08 +0200

    Fix missing return statement and update launch file default args

--- libraries/wavemap_3d/src/integrator/pointcloud_integrator.cc
+++ libraries/wavemap_3d/src/integrator/pointcloud_integrator.cc
@@ -3,5 +3,14 @@
 namespace wavemap {
 bool PointcloudIntegrator::isPointcloudValid(
     const wavemap::PosedPointcloud<wavemap::Point3D, wavemap::Transformation3D>&
-        pointcloud) {}
+        pointcloud) {
+  if (const Point3D& origin = pointcloud.getOrigin(); origin.hasNaN()) {
+    LOG(WARNING) << "Ignoring request to integrate pointcloud whose origin "
+                    "contains NaNs:\n"
+                 << origin;
+    return false;
+  }
+
+  return true;
+}
 }  // namespace wavemap
