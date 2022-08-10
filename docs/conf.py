1ae110f3f5f0dbdd508403dbcc0cc62a58adf0fe
Victor Reijgwart <victor.reijgwart@ethz-asl.ch>
2022-08-10 11:08:36 +0200

    Add draft logo

--- docs/conf.py
+++ docs/conf.py
@@ -75,6 +75,7 @@ pygments_style = None
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_logo = "logo.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
