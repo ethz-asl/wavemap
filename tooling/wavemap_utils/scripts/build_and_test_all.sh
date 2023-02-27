006616c3e9a6870da4111e6d6bfe08d11fb33bfd
Victor Reijgwart <victor_reijgwart@hotmail.com>
2023-02-27 16:43:38 +0100

    Improve local test script

--- tooling/wavemap_utils/scripts/build_and_test_all.sh
+++ tooling/wavemap_utils/scripts/build_and_test_all.sh
@@ -2,5 +2,22 @@
 
 set -o pipefail
 
-catkin build wavemap_all
-catkin test wavemap
+flags=""
+if [ "$CI" ]; then
+  flags="--no-status --force-color"
+fi
+
+catkin build wavemap_all "$flags"
+catkin build wavemap_all "$flags" --no-deps --catkin-make-args tests
+
+pushd "$(catkin locate --devel wavemap)/../.." >>/dev/null || exit 1
+all_tests_passed=1
+for f in lib/wavemap*/test_*; do
+  $f --gtest_color=yes || all_tests_passed=0
+done
+popd >>/dev/null || exit 1
+
+if [ $all_tests_passed -ne 1 ]; then
+  echo "Not all tests passed!"
+  exit 1
+fi
