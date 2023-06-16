a83fc8959991f3f4d4fef130e6dbd0dc883eb92d
Victor Reijgwart <victor.reijgwart@ethz-asl.ch>
2023-06-16 15:52:12 +0200

    Remove unused include of deprecated msg

--- ros/wavemap_ros/src/wavemap_server.cc
+++ ros/wavemap_ros/src/wavemap_server.cc
@@ -6,7 +6,6 @@
 #include <wavemap_file_conversions/file_conversions.h>
 #include <wavemap_msgs/FilePath.h>
 #include <wavemap_msgs/Map.h>
-#include <wavemap_msgs/MapEvaluationSummary.h>
 #include <wavemap_ros_conversions/config_conversions.h>
 #include <wavemap_ros_conversions/map_msg_conversions.h>
 
