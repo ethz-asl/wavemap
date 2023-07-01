b22681a39fffe7fa194395f01470c31b71636f1b
Victor Reijgwart <victor_reijgwart@hotmail.com>
2023-07-01 13:48:54 +0200

    Also source the ROS workspace in Docker img shell sessions

--- tooling/wavemap_utils/scripts/run_in_docker.sh
+++ tooling/wavemap_utils/scripts/run_in_docker.sh
@@ -5,5 +5,4 @@ docker run --rm -it \
   -e DISPLAY="${DISPLAY}" \
   -e QT_X11_NO_MITSHM=1 \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  ghcr.io/ethz-asl/wavemap:main \
-  /bin/bash
+  ghcr.io/ethz-asl/wavemap:main "$@"
