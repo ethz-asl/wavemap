d500c865a94233ababaf1db6a1d58007d2b4f807
Victor Reijgwart <victor_reijgwart@hotmail.com>
2023-07-01 21:02:33 +0200

    Automatically use Nvidia Docker runtime if available

--- tooling/wavemap_utils/scripts/run_in_docker.sh
+++ tooling/wavemap_utils/scripts/run_in_docker.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+host_data_dir_path=/home/$USER/data
+
 xhost + local:docker
-docker run --rm -it \
-  -e DISPLAY="${DISPLAY}" \
-  -e QT_X11_NO_MITSHM=1 \
-  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  ghcr.io/ethz-asl/wavemap:main "$@"
+
+if docker info | grep "Runtimes.*nvidia.*" &>/dev/null; then
+  docker run --rm -it \
+    -e DISPLAY="${DISPLAY}" \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --runtime=nvidia --gpus all \
+    -v "$host_data_dir_path":/home/ci/data:ro \
+    -e NVIDIA_VISIBLE_DEVICES=all \
+    -e NVIDIA_DRIVER_CAPABILITIES=graphics \
+    ghcr.io/ethz-asl/wavemap:main "$@"
+else
+  docker run --rm -it \
+    -e DISPLAY="${DISPLAY}" \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v "$host_data_dir_path":/home/ci/data:ro \
+    ghcr.io/ethz-asl/wavemap:main "$@"
+fi
