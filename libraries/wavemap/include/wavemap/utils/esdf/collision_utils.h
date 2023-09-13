e60fd2b77aef558f672a4478ea5daafae45cd510
Victor Reijgwart <victorreijgwart@gmail.com>
2023-09-13 23:49:44 +0200

    Add option to restrict collision free point search with AABB

--- libraries/wavemap/include/wavemap/utils/esdf/collision_utils.h
+++ libraries/wavemap/include/wavemap/utils/esdf/collision_utils.h
@@ -2,6 +2,7 @@
 #define WAVEMAP_UTILS_ESDF_COLLISION_UTILS_H_
 
 #include "wavemap/common.h"
+#include "wavemap/data_structure/aabb.h"
 #include "wavemap/data_structure/volumetric/hashed_blocks.h"
 #include "wavemap/data_structure/volumetric/volumetric_data_structure_base.h"
 #include "wavemap/utils/random_number_generator.h"
@@ -9,7 +10,8 @@
 namespace wavemap {
 std::optional<Point3D> getCollisionFreePosition(
     const VolumetricDataStructureBase& occupancy_map, const HashedBlocks& esdf,
-    FloatingPoint robot_radius);
+    FloatingPoint robot_radius,
+    std::optional<AABB<Point3D>> aabb = std::nullopt);
 }  // namespace wavemap
 
 #endif  // WAVEMAP_UTILS_ESDF_COLLISION_UTILS_H_
--- libraries/wavemap/src/utils/esdf/collision_utils.cc
+++ libraries/wavemap/src/utils/esdf/collision_utils.cc
@@ -3,28 +3,43 @@
 namespace wavemap {
 std::optional<Point3D> getCollisionFreePosition(
     const VolumetricDataStructureBase& occupancy_map, const HashedBlocks& esdf,
-    FloatingPoint robot_radius) {
+    FloatingPoint robot_radius, std::optional<AABB<Point3D>> aabb) {
   RandomNumberGenerator rng;
 
   constexpr size_t kMaxAttempts = 1000;
   for (size_t attempt_idx = 0; attempt_idx < kMaxAttempts; ++attempt_idx) {
-    const size_t nth_block =
-        rng.getRandomInteger(0ul, esdf.getBlocks().size() - 1ul);
-    auto it = esdf.getBlocks().begin();
-    std::advance(it, nth_block);
-    if (it == esdf.getBlocks().end()) {
-      continue;
+    Index3D global_index = Index3D::Zero();
+    if (aabb) {
+      const Point3D point{rng.getRandomRealNumber(aabb->min[0], aabb->max[0]),
+                          rng.getRandomRealNumber(aabb->min[1], aabb->max[1]),
+                          rng.getRandomRealNumber(aabb->min[2], aabb->max[2])};
+      global_index =
+          convert::pointToNearestIndex(point, 1.f / esdf.getMinCellWidth());
+    } else {
+      const size_t nth_block =
+          rng.getRandomInteger(0ul, esdf.getBlocks().size() - 1ul);
+      auto it = esdf.getBlocks().begin();
+      std::advance(it, nth_block);
+      if (it == esdf.getBlocks().end()) {
+        continue;
+      }
+
+      const LinearIndex linear_cell_index =
+          rng.getRandomInteger(0, HashedBlocks::kCellsPerBlock - 1);
+
+      const Index3D& block_index = it->first;
+      const Index3D cell_index =
+          convert::linearIndexToIndex<HashedBlocks::kCellsPerSide, 3>(
+              linear_cell_index);
+      global_index =
+          esdf.computeIndexFromBlockIndexAndCellIndex(block_index, cell_index);
     }
 
-    const LinearIndex linear_cell_index =
-        rng.getRandomInteger(0, HashedBlocks::kCellsPerBlock - 1);
-
-    const Index3D& block_index = it->first;
-    const Index3D cell_index =
-        convert::linearIndexToIndex<HashedBlocks::kCellsPerSide, 3>(
-            linear_cell_index);
-    const Index3D global_index =
-        esdf.computeIndexFromBlockIndexAndCellIndex(block_index, cell_index);
+    Point3D position =
+        convert::indexToCenterPoint(global_index, esdf.getMinCellWidth());
+    if (aabb && !aabb->containsPoint(position)) {
+      continue;
+    }
 
     const FloatingPoint occupancy_value =
         occupancy_map.getCellValue(global_index);
@@ -33,15 +48,12 @@ std::optional<Point3D> getCollisionFreePosition(
       continue;
     }
 
-    const auto& block = it->second;
-    const FloatingPoint esdf_value = block[linear_cell_index];
+    const FloatingPoint esdf_value = esdf.getCellValue(global_index);
     if (esdf_value < robot_radius) {
       continue;
     }
 
-    Point3D collision_free_position =
-        convert::indexToCenterPoint(global_index, esdf.getMinCellWidth());
-    return collision_free_position;
+    return position;
   }
 
   LOG(WARNING) << "Could not find collision free position. Giving up after "
