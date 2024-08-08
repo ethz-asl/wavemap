f3db2f84db480cee122fd417ccb8a343ce8a95f3
Victor Reijgwart <victorreijgwart@gmail.com>
2024-08-08 16:00:05 +0200

    Initial commit for wavemap's Python API

---
+++ library/python/pywavemap.cc
@@ -0,0 +1,45 @@
+#include <nanobind/eigen/dense.h>
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/filesystem.h>
+#include <nanobind/stl/shared_ptr.h>
+#include <wavemap/core/map/map_base.h>
+#include <wavemap/io/file_conversions.h>
+
+using namespace wavemap;  // NOLINT
+namespace nb = nanobind;
+using namespace nb::literals;  // NOLINT
+
+NB_MODULE(pywavemap, m) {
+  m.doc() =
+      "A fast, efficient and accurate multi-resolution, multi-sensor 3D "
+      "occupancy mapping framework.";
+
+  nb::class_<MapBase>(m, "Map")
+      .def_prop_ro("empty", &MapBase::empty)
+      .def_prop_ro("size", &MapBase::size)
+      .def("threshold", &MapBase::threshold)
+      .def("prune", &MapBase::prune)
+      .def("pruneSmart", &MapBase::pruneSmart)
+      .def("clear", &MapBase::clear)
+      .def_prop_ro("min_cell_width", &MapBase::getMinCellWidth)
+      .def_prop_ro("min_log_odds", &MapBase::getMinLogOdds)
+      .def_prop_ro("max_log_odds", &MapBase::getMaxLogOdds)
+      .def_prop_ro("memory_usage", &MapBase::getMemoryUsage)
+      .def_prop_ro("tree_height", &MapBase::getTreeHeight)
+      .def_prop_ro("min_index", &MapBase::getMinIndex)
+      .def_prop_ro("max_index", &MapBase::getMaxIndex)
+      .def("getCellValue", &MapBase::getCellValue)
+      .def("setCellValue", &MapBase::setCellValue)
+      .def("addToCellValue", &MapBase::addToCellValue);
+
+  m.def(
+      "load_map",
+      [](const std::filesystem::path& file_path) -> std::shared_ptr<MapBase> {
+        wavemap::MapBase::Ptr map;
+        if (wavemap::io::fileToMap(file_path, map)) {
+          return map;
+        }
+        return nullptr;
+      },
+      "file_path"_a, "Load a wavemap map from a .wvmp file.");
+}
