2d2ca39da96e403955ed139fc084e9ed46049faa
Victor Reijgwart <victorreijgwart@gmail.com>
2024-08-15 16:47:28 +0200

    Explicitly import all members to avoid linting errors in user pkgs

--- library/python/src/pywavemap/__init__.py
+++ library/python/src/pywavemap/__init__.py
@@ -1,1 +1,7 @@
-from .pywavemap import *
+from .pywavemap import Map
+from .pywavemap import Pipeline
+from .pywavemap import Rotation, Pose
+from .pywavemap import Pointcloud, PosedPointcloud
+from .pywavemap import Image, PosedImage
+from .pywavemap import logging
+from .pywavemap import param
