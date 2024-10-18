776bfe2b9e176a17f349f7f6fa76c9d62cd0a0b1
Sergei Sergienko <divelix1@gmail.com>
2024-10-18 18:55:59 +0300

    feat: Added depth extraction functionality

--- library/python/src/pywavemap/__init__.py
+++ library/python/src/pywavemap/__init__.py
@@ -9,6 +9,7 @@ from ._pywavemap_bindings import (Map, HashedWaveletOctree,
                                   HashedChunkedWaveletOctree,
                                   InterpolationMode)
 from ._pywavemap_bindings import Pipeline
+from ._pywavemap_bindings import raycast, PinholeCameraProjectorConfig, get_depth
 
 # Binding submodules
 from ._pywavemap_bindings import logging, param, convert
